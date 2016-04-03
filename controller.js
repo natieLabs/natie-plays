c0c131f1913d7234a805857b3629af73066e5bd2
Annie Jiao <anniej@mit.edu>
2016-04-03 11:06:55 +0300

    logo url

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ $(function() {
     $('body').on('touchmove', function(e) {
         e.preventDefault();
     });
-    var logoHTML = '<div class="ui large secondary menu"> <a class="ui image item" href="http://natie.com"><img src="/logo.png" class="logo"></img> </a> <div class="right menu"> <div class="ui item"> <h5>NATIE/LABS/PLAY</h5></div> </div> </div>'
+    var logoHTML = '<div class="ui large secondary menu"> <a class="ui image item" href="http://natie.com"><img src="https://raw.githubusercontent.com/natieLabs/natie-plays/gh-pages/logo.png" class="logo"></img> </a> <div class="right menu"> <div class="ui item"> <h5>NATIE/LABS/PLAY</h5></div> </div> </div>'
     $("body").prepend(logoHTML);
 
     var navHTML = '<div class="ui nav stackable centered equal width grid container"><div class="nav two wide column home"> <a href="/index.html"> <div class="router home"> <h2> BACK </h2> <div class="bottom bar"></div> </div> </a> </div>';
