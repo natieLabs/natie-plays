b8b5c6b2d573ce0f2d0f94c9cb4ef0a1d2076f4a
Annie Jiao <anniej@mit.edu>
2016-04-06 22:25:06 -0400

    refactored for OOP

--- arkanoid/arkanoid.js
+++ arkanoid/arkanoid.js
@@ -1,5 +1,93 @@
 $(function() {
 
+    var container = $("#container");
+    var xmin = container.offset().left;
+    var xmax = container.offset().left + container.width();
+    var side = container.width();
+    var top = container.offset().top;
+
+    $(".buttons").offset({ left: xmax + 10, top: top });
+
+    var lifes = 3;
+    var score = 0;
+
+    $("#lifesNode").html(lifes);
+
+    var ball = {
+        r: 5,
+        bx: 100,
+        by: 100,
+        dx: -4,
+        dy: -4,
+        self: $("#ball"),
+        row: null,
+        col: null,
+
+        init: function() {
+            this.self.width(2 * this.r);
+            this.self.height(2 * this.r);
+            this.row = (ball.by / bricks.outerHeight) | 0;
+            this.col = (ball.bx / bricks.widthWithMargin) | 0;
+        },
+
+        getRow: function() {
+            return (ball.by / bricks.outerHeight) | 0;
+        },
+
+        getCol: function() {
+            return (ball.bx / bricks.widthWithMargin) | 0;
+        },
+
+        update: function() {
+            this.bx = (this.bx + this.dx) | 0;
+            this.by = (this.by + this.dy) | 0;
+            this.row = (ball.by / bricks.outerHeight) | 0;
+            this.col = (ball.bx / bricks.widthWithMargin) | 0;
+
+
+            this.self.css({ left: this.bx, top: this.by });
+            // this.put(this.bx, this.by);
+            if (this.bx < 0 && this.dx < 0 || this.bx >= side - this.r && this.dx > 0) {
+                this.flipx();
+            }
+            // hits paddle
+            if (this.bx >= paddle.px && this.bx <= paddle.px + paddle.width && this.by + 2 * this.r >= paddle.top && this.by < paddle.bottom) {
+                this.flipy();
+                if (this.bx <= paddle.px + paddle.width / 4) { // hits left half, move left
+                    this.dx = -6;
+                } else if (this.bx >= paddle.px + paddle.width / 4 * 3) { // hits right half, move right
+                    this.dx = 6;
+                } else if (Math.abs(this.dx) === 6) { // hits middle, slowdown
+                    this.dx = (this.dx * 2 / 3) | 0;
+                }
+            }
+
+            // hits top
+            if (this.by <= 0) { this.dy *= -1; }
+
+            // check game ends
+            if (this.by >= side - this.r * 2 && !--lifes) {
+                $("#lifesNode").html(lifes);
+                addGameOver();
+                clearInterval(cycle);
+            };
+
+            // hits bottom
+            if (this.by + this.r * 2 >= side && lifes) {
+                this.dy *= -1;
+                $("#lifesNode").html(lifes);
+            }
+        },
+
+        flipx: function() {
+            this.dx *= -1;
+        },
+
+        flipy: function() {
+            this.dy *= -1;
+        }
+    }
+
     // object for set of bricks
     var bricks = {
         NUM_BRICKS: 50,
@@ -24,7 +112,7 @@ $(function() {
             }
             this.side = container.width();
             this.array = container.children(".brick");
-            this.widthWithMargin = ~~(side / this.BRICKS_PER_ROW);
+            this.widthWithMargin = side / this.BRICKS_PER_ROW;
             this.width = this.widthWithMargin - this.BRICK_MARGIN * 2;
             $(".brick").width(this.width);
             this.baseline = this.array.last().position().top + this.array.last().height();
@@ -35,144 +123,79 @@ $(function() {
             var index = r * this.BRICKS_PER_ROW + c;
             return $("#brick" + index);
         },
+
+        isRemoved: function(r, c) {
+            return this.get(r, c).hasClass("removed");
+        },
+
+        remove: function(r, c) {
+            this.get(r, c).addClass("removed");
+        },
+
+        update: function(ball) {
+            if (ball.by >= 0 && ball.by < bricks.baseline) {
+
+                if (!bricks.isRemoved(ball.row, ball.col)) {
+                    ball.dy *= -1;
+                    bricks.remove(ball.row, ball.col);
+
+                    // bounces on left and right sides of bricks, flip x direction
+                    if (ball.dx < 0 && (ball.bx % bricks.widthWithMargin < ball.r || ball.bx % bricks.widthWithMargin > (ball.r * 2 + 2))) {
+                        // console.log("hi");
+                        ball.dx *= -1;
+                    }
+                    if (ball.dx > 0 && ((ball.bx + ball.r * 2) % bricks.widthWithMargin < ball.r || (ball.bx + ball.r * 2) % bricks.widthWithMargin > (ball.r * 2))) {
+                        // console.log("hi");
+                        ball.dx *= -1;
+                    }
+                    $("#scoreNode").html(++score);
+
+                    //YOU WIN
+                    if (score == bricks.NUM_BRICKS) {
+                        addGameOver();
+                        clearInterval(cycle);
+                    };
+                }
+            }
+        }
     }
 
-    // var ball = {
-    //     R: 5,
-    //     bx: 0,
-    //     by: 0,
-    //     dx: -4,
-    //     dy: -4,
-    //     self: $("#ball"),
+    var paddle = {
+        px: 129,
+        width: null,
+        self: $("#paddle"),
+        top: null,
+        bottom: null,
+        height: 8,
 
-    //     init: function() {
-    //       self.width(2*r);
-    //       self.height(2*r);
-    //     },
+        init: function() {
+            this.top = this.self.position().top;
+            this.bottom = this.top + this.height;
+            this.width = side / 5;
+            this.self.css({ left: this.px, width: this.width });
+        },
 
-    //     put: function(x, y){
-    //       // self.position().top;
-    //       ball.css({ left: x, top: y });
-    //       this.bx = x;
-    //       thix.by = y;
-    //     }
-
-    //     update: function(){
-    //       this.bx = (this.bx + dx) | 0;
-    //       this.by = (this.by + dy) | 0;
-    //       put(this.bx, this.by);
-    //     }
-    // }
-
-    var container = $("#container");
-    var xmin = container.offset().left;
-    var xmax = container.offset().left + container.width();
-    var side = container.width();
-    var top = container.offset().top;
-
-    $(".buttons").offset({ left: xmax + 10, top: top })
-
-    var px = 129,
-        dx = -4,
-        dy = -4,
-        lifes = 3,
-        score = 0,
-        bx = by = 0;
-
-    $("#lifesNode").html(lifes);
-    var paddle = $("#paddle");
-    var ball = $("#ball");
-
-    const r = 5;
-    const PADDLE_WIDTH = side / 5;
-    const PADDLE_TOP = paddle.position().top;
-    const PADDLE_BOTTOM = PADDLE_TOP + paddle.height();
+        update: function(e) {
+            var x = (e.type == "mousemove") ? e.pageX : e.originalEvent.touches[0].pageX;
+            var left_bound = xmin + this.width / 2;
+            var right_bound = xmax - this.width / 2;
+            this.px = (x > left_bound) ? ((x < right_bound) ? x - left_bound : xmax - xmin - this.width) : 0;
+            this.self.css({ left: this.px });
+        }
+    }
 
     bricks.init(container);
-
-    ball.width(2 * r);
-    ball.height(2 * r);
-    paddle.width(PADDLE_WIDTH);
+    ball.init();
+    paddle.init();
 
     var cycle = setInterval(function() {
-        bx = (ball.position().left + dx) | 0;
-        by = (ball.position().top + dy) | 0;
+        ball.update();
+        bricks.update(ball);
 
-        var row = (by / bricks.outerHeight) | 0;
-        var col = (bx / bricks.widthWithMargin) | 0;
-
-        // update ball location
-        ball.css({ left: bx, top: by });
-
-        // hits left right sides, reverse x direction
-        if (bx < 0 && dx < 0 || bx >= side - r && dx > 0) {
-            dx *= -1;
-        }
-        // hits paddle
-        if (bx >= px && bx <= px + PADDLE_WIDTH && by + 2 * r >= PADDLE_TOP && by < PADDLE_BOTTOM) {
-            dy *= -1;
-            if (bx <= px + PADDLE_WIDTH / 4) { // hits left half, move left
-                dx = -6;
-            } else if (bx >= px + PADDLE_WIDTH / 4 * 3) { // hits right half, move right
-                dx = 6;
-            } else if (Math.abs(dx) === 6) { // hits middle, slowdown
-                dx = (dx * 2 / 3) | 0;
-            }
-        }
-
-        // hits top
-        if (by <= 0) { dy *= -1; }
-
-        // check game ends
-        if (by >= side - r * 2 && !--lifes) {
-            $("#lifesNode").html(lifes);
-            addGameOver();
-            clearInterval(cycle);
-        };
-
-        // hits bottom
-        if (by + r * 2 >= side && lifes) {
-            dy *= -1;
-            $("#lifesNode").html(lifes);
-        }
-
-        // remove bricks
-        if (by >= 0 && by < bricks.baseline) {
-
-            var isRemoved = bricks.get(row, col).hasClass("removed");
-            if (!isRemoved) {
-                dy *= -1;
-                bricks.get(row, col).addClass('removed');
-
-                // bounces on left and right sides of bricks, flip x direction
-                if (dx < 0 && (bx % bricks.widthWithMargin < r || bx % bricks.widthWithMargin > (r * 2 + 2))) {
-                    dx *= -1;
-                }
-                if (dx > 0 && ((bx + r * 2) % bricks.widthWithMargin < r || (bx + r * 2) % bricks.widthWithMargin > (r * 2))) {
-                    dx *= -1;
-                }
-                $("#scoreNode").html(++score);
-
-                //YOU WIN
-                if (score == bricks.NUM_BRICKS) {
-                    addGameOver();
-                    clearInterval(cycle);
-                };
-            }
-        }
     }, 1000 / 60);
 
     $(document).bind("mousemove touchmove", function(e) {
-        handleMove(e);
+        paddle.update(e);
     })
-
-    function handleMove(e) {
-        var x = (e.type == "mousemove") ? e.pageX : e.originalEvent.touches[0].pageX;
-        var left_bound = xmin + $("#paddle").width() / 2;
-        var right_bound = xmax - $("#paddle").width() / 2;
-        px = (x > left_bound) ? ((x < right_bound) ? x - left_bound : xmax - xmin - $("#paddle").width()) : 0;
-        $("#paddle").css({ left: px });
-    }
-
 
 });
